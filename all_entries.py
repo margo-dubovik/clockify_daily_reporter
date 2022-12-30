d2b67e3ab789b1384187c5db1f1bc586c1bbb30b
marharytadubovyk <margodubovik531@gmail.com>
2022-12-30 17:36:29 +0200

    comment added

--- all_entries.py
+++ all_entries.py
@@ -11,9 +11,7 @@ USER_ID = os.getenv('USER_ID')
 
 headers = {'x-api-key': API_KEY}
 
-
 url = f'https://api.clockify.me/api/v1/workspaces/{WORKSPACE_ID}/user/{USER_ID}/time-entries'
 all_entries = requests.get(url, headers=headers).json()
-all_entries_json = json.dumps(all_entries, indent=2, ensure_ascii=False)
+all_entries_json = json.dumps(all_entries, indent=2, ensure_ascii=False)  # for pretty printing
 print(all_entries_json)
-
