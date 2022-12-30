84bc9db26f7f45b965c8c38c9385315e787691ae
marharytadubovyk <margodubovik531@gmail.com>
2022-12-30 17:35:42 +0200

    summary report added

---
+++ summary_report.py
@@ -0,0 +1,37 @@
+import json
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+WORKSPACE_ID = os.getenv('WORKSPACE_ID')
+PROJECT_ID = os.getenv('PROJECT_ID')
+USER_ID = os.getenv('USER_ID')
+
+headers = {'x-api-key': API_KEY,
+           "content-type": "application/json"}
+
+data = {
+    "dateRangeStart": "2022-12-29T00:00:00.000",
+    "dateRangeEnd": "2022-12-30T23:59:59.000",
+    "summaryFilter": {
+        "groups": [
+            'DATE',
+            'TIMEENTRY',
+        ],
+        'sortColumn': 'GROUP',
+        "options": {
+            "totals": "CALCULATE"
+        },
+    },
+    "sortOrder": "ASCENDING",
+    'amountShown': 'HIDE_AMOUNT',
+    "exportType": "JSON",
+}
+
+url = f'https://reports.api.clockify.me/v1/workspaces/{WORKSPACE_ID}/reports/summary'
+summary_report = requests.post(url, headers=headers, json=data).json()
+summary_report_json = json.dumps(summary_report, indent=3, ensure_ascii=False)  # for pretty printing
+print(summary_report_json)
